374c5d7ed1710aea65f3406aa506b589d7ebf533
Sebastian Klüft <skluft@gmail.com>
2013-11-24 20:37:40 +0100

    Restructure stuff

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,15 @@
+import os.path
 import cherrypy
-class HelloWorld(object):
-    def index(self):
-        return "Hello World!"
-    index.exposed = True
+from cherrypy.lib.static import serve_file
 
-cherrypy.quickstart(HelloWorld())
+class Root(object):
+    @cherrypy.expose
+    def default(self, *args, **kwargs):
+        if len(args) == 0:
+            return serve_file(os.path.join(current_dir, 'app', 'index.html'))
+        else:
+            return serve_file(os.path.join(current_dir, 'app', *args))
+
+if __name__ == '__main__':
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    cherrypy.quickstart(Root(), '/')
